91776c14fff53e24bea3615c7a5876b605e853cd
wlsgh7608 <wlsgh7608@naver.com>
2022-01-25 13:10:18 +0900

    Django 초기 세팅 설정

---
+++ base/base/settings/local.py
@@ -0,0 +1,7 @@
+from .common import *
+
+if DEBUG: #debug 환경이면 실행
+    pass
+	# INSTALLED_APPS += ['debug_toolbar', ]
+	
+    # MIDDLEWARE += ["debug_toolbar.middleware.DEbugToolbarMiddleware",]
---
+++ base/base/settings/production.py
@@ -0,0 +1,16 @@
+from .common import *
+
+
+DATABASES = {
+    'default': {
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sample',
+        'USER': 'local',
+        'PASSWORD': '1234',
+
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
+    }
+}
